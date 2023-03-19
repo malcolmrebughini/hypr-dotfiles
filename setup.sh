c2026df76d9439678d9b3d2e52315899e936e85b
Malcolm <9681621+malcolmrebughini@users.noreply.github.com>
2023-03-19 13:32:16 -0700

    feat(eww): using netctl for wifi tooltip

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ makepkg -si
 cd $curr
 
 # Base
-sudo pacman -S gnome-keyring alacritty nushell starship zoxide rustup grim flameshot polkit-kde-agent dunst pamixer libpulse xdg-desktop-portal wireplumber socat nvidia-dkms swayidle slurp papirus-icon-theme socat nvidia-dkms firefox thunar pipewire-pulse pipewire pipewire-jack helvum rofi dmenu mypaint spotify ttf-fira-code ttf-jetbrains-mono
+sudo pacman -S netctl gnome-keyring alacritty nushell starship zoxide rustup grim flameshot polkit-kde-agent dunst pamixer libpulse xdg-desktop-portal wireplumber socat nvidia-dkms swayidle slurp papirus-icon-theme socat nvidia-dkms firefox thunar pipewire-pulse pipewire pipewire-jack helvum rofi dmenu mypaint spotify ttf-fira-code ttf-jetbrains-mono adobe-source-han-sans-jp-fonts 
 paru -S eww-wayland swww vscodium-bin greetd greetd-wlgreet xdg-desktop-portal-hyprland-git swaylock-effects hyprland-nvidia-git spotify bibata-cursor-theme
 
 
