ba4517c791e59b5849294f5fdb7f136f1422055d
Malcolm <9681621+malcolmrebughini@users.noreply.github.com>
2023-03-19 22:18:16 -0700

    feat(eww): add topbar config

--- hyprstart.sh
+++ hyprstart.sh
@@ -27,4 +27,21 @@ export GBM_BACKEND=nvidia-drm
 export __GLX_VENDOR_LIBRARY_NAME=nvidia
 export WLR_NO_HARDWARE_CURSORS=1
 
+# v = vertical 
+# h = horizontal
+export BAR_ORIENTATION="h"
+
+
+if [[ $BAR_ORIENTATION = "v" ]]
+then
+    export TOP_RESERVED="0"
+    export LEFT_RESERVED="50"
+    export BAR_EWW="leftbar"
+else
+    export TOP_RESERVED="45"
+    export LEFT_RESERVED="0"
+    export BAR_EWW="topbar"
+fi
+
+
 exec Hyprland
